0df463eb95368597313e617cbdc86ecdedde851a
kentarokoizumi <kentaroskoizumi@gmail.com>
2023-08-03 12:52:56 +0900

    デプロイの自動化を試みた

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES  // この行を追加
-  ? "reactailwind"            // この行を追加
+  ? "uniplanner"            // この行を追加
   : "./",                     // この行を追加
   plugins: [react()],
 })
