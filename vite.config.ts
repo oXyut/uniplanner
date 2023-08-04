215646d5f9614d332db68ccfd8956f04f887a85c
kentarokoizumi <kentaroskoizumi@gmail.com>
2023-08-04 10:46:22 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES  // この行を追加
-  ? "reactailwind"            // この行を追加
+  ? "uniplanner-test"            // この行を追加
   : "./",                     // この行を追加
   plugins: [react(), tsconfigPaths()],
 })
