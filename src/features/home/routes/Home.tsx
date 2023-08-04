c9aa61f13edac84546b37c2ce326bc17160284cf
kentarokoizumi <kentaroskoizumi@gmail.com>
2023-08-04 19:44:40 +0900

    トップページの作成

--- src/features/home/routes/Home.tsx
+++ src/features/home/routes/Home.tsx
@@ -1,8 +1,18 @@
+import { Button } from '@/components/Elements/Button'
+import { Head } from '@/components/Head'
 export const Home = () => {
   return (
     <>
-      <div className="text-center bg-gray-800 w-full h-screen flex items-center justify-center">
-        <p className="text-white text-9xl">UniPlanner</p>
+      <Head title="Home" description="Home" />
+      <div className="text-center w-full h-screen flex flex-col items-center justify-center gap-4">
+        <p className='text-light font-bold'>時間割ベースの日程調整アプリ</p>
+        <p className="text-light text-9xl mb-3">UniPlanner</p>
+        <Button
+          variant='primary'
+          onClick={() => {console.log('clicked')}}
+        >
+          新しいイベントを作成
+        </Button>
       </div>
     </>
   )
